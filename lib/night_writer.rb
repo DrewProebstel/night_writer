9d635d42c8182892e85878f25252f5bab1465c0e
Drew Proebstel <d.proebstel@gmail.com>
2022-03-04 16:13:22 -0800

    add count lines

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -10,4 +10,12 @@ writer = File.open(ARGV[1], "w")
 
 writer.write(capitalized_test)
 
-write.close
+def count_lines
+  lines = File.readlines(ARGV[0])
+  line_count = lines.size
+  text = lines.join
+  total_characters = text.length
+  puts "#{total_characters} characters"
+end
+writer.close
+count_lines
