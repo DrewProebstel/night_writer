c07f843e97454597860aef41c9170ec21ea10605
Drew Proebstel <d.proebstel@gmail.com>
2022-03-04 16:21:05 -0800

    put night_writer into methods

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,21 +1,23 @@
-handle = File.open(ARGV[0], "r")
+def get_message
+  handle = File.open(ARGV[0], "r")
+  incoming_text = handle.read
+  handle.close
+  incoming_text
+end
 
-incoming_text = handle.read
-
-handle.close
-
-capitalized_test = incoming_text.upcase
-
-writer = File.open(ARGV[1], "w")
-
-writer.write(capitalized_test)
+def write
+  capitalized_test = get_message.upcase
+  writer = File.open(ARGV[1], "w")
+  writer.write(capitalized_test)
+end
 
 def count_lines
   lines = File.readlines(ARGV[0])
   line_count = lines.size
   text = lines.join
-  total_characters = text.length
-  puts "#{total_characters} characters"
+  text.length
 end
-writer.close
-count_lines
+
+get_message
+write
+puts "Created '#{ARGV[1]}'' containing #{count_lines} characters"
