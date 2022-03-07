3559920ce8660bde2ffb090d4f5394a12e5b1e86
Drew Proebstel <d.proebstel@gmail.com>
2022-03-07 09:59:33 -0800

    git up to date

---
+++ lib/translator.rb
@@ -0,0 +1,23 @@
+require './lib/night_writer.rb'
+require './lib/alphabet.rb'
+require 'pry'
+
+class Translator
+  attr_accessor :text, :top_array, :bottom_array, :middle_array, :alphabet
+  def initialize(input,alphabet)
+    @text = input.get_message.tr("\n", " ")
+    @top_array = []
+    @middle_array = []
+    @bottom_array = []
+    @alphabet = alphabet
+  end
+
+  def translate
+    text.length do |letter|
+      top_array.push(alphabet.alphabet[letter.downcase][0])
+      middle_array.push(alphabet.alphabet[letter.downcase][1])
+      bottom_array.push(alphabet.alphabet[letter.downcase][2])
+    end
+  end
+
+end
