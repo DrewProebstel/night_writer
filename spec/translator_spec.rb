13461dc4345e5a274b5d3953f15d8499a04873a0
Drew Proebstel <d.proebstel@gmail.com>
2022-03-08 14:20:09 -0800

    compile array test

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -24,4 +24,13 @@ RSpec.describe Translator do
     expect(text.middle_array).to eq([[".", "."], ["0", "."], [".", "."], [".", "."]])
     expect(text.bottom_array).to eq([[".", "."], [".", "."], [".", "."], [".", "."]])
   end
+
+  it "compile_array" do
+    arguments = ['abc.txt', 'foo.txt']
+    message = Night_writer.new(arguments)
+    alphabet = Alphabet.new
+    text = Translator.new(message,alphabet)
+    text.translate
+    expect(text.compile_array).to eq("0.0.00..\n..0.....\n........\n\n")
+  end
 end
